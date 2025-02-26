310164627ec5116e1740534757f6dc088834aaa4
devmodes <dev.clarkdave@gmail.com>
2025-02-26 20:39:57 +0800

    Updated the constants config in the ip management service

--- ip-management-service/src/config/app.ts
+++ ip-management-service/src/config/app.ts
@@ -1,3 +1,10 @@
 export default {
   port: process.env.APP_PORT,
-}
+
+  // Constants
+  create_ip: "create:ip",
+  read_ip: "read:ip",
+  update_ip: "update:ip",
+  update_ip_label: "update:ip-label",
+  delete_ip: "delete:ip",
+};
